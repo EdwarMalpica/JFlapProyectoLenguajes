3585c676fadb4e41af36913baba8226b8b0c4ffc
Luigi-code677 <70174799+Luigi-code677@users.noreply.github.com>
2022-12-13 16:18:15 -0500

    Cambio de color en el NFA

--- NDFA_GUI-master/src/views/elements/Transition.java
+++ NDFA_GUI-master/src/views/elements/Transition.java
@@ -14,18 +14,23 @@ public class Transition {
 	private int index = 0;
 
 	public void drawTransition(Graphics g) {
-		g.setColor(Color.WHITE);
-		if(end != null && start != null)
-			g.drawLine(start.x + GraphicsFA.CIRCLE_RADIUS/2, start.y + GraphicsFA.CIRCLE_RADIUS/2, end.x + GraphicsFA.CIRCLE_RADIUS/2, end.y + GraphicsFA.CIRCLE_RADIUS/2);
+		g.setColor(Color.BLACK);
+		if (end != null && start != null)
+			g.drawLine(start.x + GraphicsFA.CIRCLE_RADIUS / 2, start.y + GraphicsFA.CIRCLE_RADIUS / 2,
+					end.x + GraphicsFA.CIRCLE_RADIUS / 2, end.y + GraphicsFA.CIRCLE_RADIUS / 2);
 	}
+
 	public void drawCondition(Graphics g) {
 		Graphics2D gr = (Graphics2D) g;
-		if(!condicion.isEmpty()) {
-			gr.drawString(condicion, ( start.x + end.x ) / 2 + GraphicsFA.CIRCLE_RADIUS/2,  ((start.y + end.y) / 2 + GraphicsFA.CIRCLE_RADIUS/2) - (index * 15));
-		}else {
-			gr.drawString("λ", ( start.x + end.x ) / 2 + GraphicsFA.CIRCLE_RADIUS/2,  ((start.y + end.y) / 2 + GraphicsFA.CIRCLE_RADIUS/2) - (index * 15));
+		if (!condicion.isEmpty()) {
+			gr.drawString(condicion, (start.x + end.x) / 2 + GraphicsFA.CIRCLE_RADIUS / 2,
+					((start.y + end.y) / 2 + GraphicsFA.CIRCLE_RADIUS / 2) - (index * 15));
+		} else {
+			gr.drawString("λ", (start.x + end.x) / 2 + GraphicsFA.CIRCLE_RADIUS / 2,
+					((start.y + end.y) / 2 + GraphicsFA.CIRCLE_RADIUS / 2) - (index * 15));
 		}
 	}
+
 	public void setIndex(int index) {
 		this.index = index;
 	}
